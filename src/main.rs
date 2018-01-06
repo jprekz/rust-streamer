9ad1dcf66de7bf9d39f83be8fe7f0e8d73223ffa
jprekz <jprekz@users.noreply.github.com>
2018-01-07 04:11:18 +0900

    working

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,26 @@ use rust_streamer::sample::*;
 use rust_streamer::graphic::*;
 
 fn main() {
-    //let source = WAVSource::new("test85.wav");
+    let source: WAVSource<Stereo<f64>> = WAVSource::new("test85.wav");
     //let source = FreqConv::new(source);
 
-    let osc = SineWave::new(440.0);
+    let osc: SineWave<Stereo<f64>> = SineWave::new(440.0);
 
     let bc = FnElement::new(|x: Stereo<i16>| {
         x.map(|s: i16| { s & !0x1fff })
     });
 
-    let oscillo = Oscillo::new(640);
-    //let spectrum = Spectrum::new(1024*2);
-    let oscillo2 = Oscillo::new(640);
-    //let spectrum2 = Spectrum::new(1024*2);
-
-    let mut b = [Stereo { l: 0, r: 0 }; 8];
-    let lp = FnElement::new(move |x: Stereo<i16>| {
-        b.push(x);
-        Stereo::<i16> {
-            l: b.iter().map(|s| { s.l / b.len() as i16 }).sum(),
-            r: b.iter().map(|s| { s.r / b.len() as i16 }).sum()
-        }
-    });
+    //let oscillo = Oscillo::new(640);
+    let spectrum = Spectrum::new(1024*2);
+    //let oscillo2 = Oscillo::new(640);
+    let spectrum2 = Spectrum::new(1024*2);
 
     //let tee = Tee::new(|x| {println!("{:?}", x)});
 
     //let sink = PrintSink::new();
     let sink = CpalSink::new();
 
-    let p = pipe!(osc, oscillo, bc, lp, oscillo2, sink);
+    let p = pipe!(source, spectrum, LowPassFilter::new(440.0, 2.0), spectrum2, sink);
 
     p.start(&Context::new(44100));
 }
