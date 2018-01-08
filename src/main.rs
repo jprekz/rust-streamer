2c3470c69e0aa5f3fd52796812026dedf1bff857
jprekz <jprekz@users.noreply.github.com>
2018-01-09 00:36:31 +0900

    clean

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,24 @@ use rust_streamer::graphic::*;
 
 fn main() {
     let source: WAVSource<Stereo<f64>> = WAVSource::new("test85.wav");
-    //let source = FreqConv::new(source);
-
-    let osc: SineWave<Stereo<f64>> = SineWave::new(440.0);
 
     let bc = FnElement::new(|x: Stereo<i16>| {
         x.map(|s: i16| { s & !0x1fff })
     });
-
-    //let oscillo = Oscillo::new(640);
-    let spectrum = Spectrum::new(1024*2);
-    //let oscillo2 = Oscillo::new(640);
-    let spectrum2 = Spectrum::new(1024*2);
 
     //let tee = Tee::new(|x| {println!("{:?}", x)});
 
     //let sink = PrintSink::new();
     let sink = CpalSink::new();
 
-    let p = pipe!(source, spectrum, LowPassFilter::new(440.0, 2.0), spectrum2, sink);
+    let p = pipe!(
+        source,
+        Spectrum::new(1024),
+        PeakingFilter::new(1000.0, 2.0, -30.0),
+        Spectrum::new(1024),
+        Oscillo::new(640),
+        sink
+    );
 
     p.start(&Context::new(44100));
 }
