b0e0c6e39f6f728b756647ae457543cc9580fc21
jprekz <jprekz@users.noreply.github.com>
2018-01-15 12:20:42 +0900

    exampleをexamplesにまとめた

---
+++ examples/ex1.rs
@@ -0,0 +1,13 @@
+extern crate rust_streamer;
+use rust_streamer::*;
+use rust_streamer::element::*;
+use rust_streamer::sample::*;
+
+fn main() {
+    let source = SineWave::<Stereo<f64>>::new(440.0);
+    let sink = DefaultSink::new();
+
+    let p = pipe!(source, sink);
+
+    p.start(&Context::new(44100));
+}
---
+++ examples/ex2.rs
@@ -0,0 +1,17 @@
+extern crate rust_streamer;
+use rust_streamer::*;
+use rust_streamer::element::*;
+use rust_streamer::sample::*;
+use rust_streamer::graphic::*;
+
+fn main() {
+    let p = pipe!(
+        WAVSource::<Stereo<f64>>::new("examples/test85.wav"),
+        Spectrum::new(1024),
+        LowPassFilter::new(1000.0, 1.0 / 2f64.sqrt()),
+        Spectrum::new(1024),
+        DefaultSink::new()
+    );
+
+    p.start(&Context::new(44100));
+}
